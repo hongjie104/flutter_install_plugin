779353cc91ea1997c4e99211ff810269d76466a8
hongjie <32968210@qq.com>
2019-04-28 14:17:06 +0800

    rm swift

--- ios/Classes/InstallPlugin.m
+++ ios/Classes/InstallPlugin.m
@@ -1,8 +1,7 @@
 #import "InstallPlugin.h"
-#import <install_plugin/install_plugin-Swift.h>
 
 @implementation InstallPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftInstallPlugin registerWithRegistrar:registrar];
+  // do nothing
 }
 @end
