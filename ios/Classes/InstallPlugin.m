e439509845c6763ac1a8b204323f509675ddefd6
hongjie <32968210@qq.com>
2019-04-28 14:51:45 +0800

    fixed a bug

--- ios/Classes/InstallPlugin.m
+++ ios/Classes/InstallPlugin.m
@@ -5,13 +5,14 @@
   FlutterMethodChannel* channel = [FlutterMethodChannel
       methodChannelWithName:@"install_plugin"
             binaryMessenger:[registrar messenger]];
+  InstallPlugin* instance = [[InstallPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"gotoAppStore" isEqualToString:call.method]) {
     // result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-    result[@"success"];
+    result([@"success"]);
   } else {
     result(FlutterMethodNotImplemented);
   }
