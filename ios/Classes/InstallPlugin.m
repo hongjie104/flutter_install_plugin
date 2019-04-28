911dddeeddbb947480d99fb0221c165aa5f34d7a
hongjie <32968210@qq.com>
2019-04-28 14:37:44 +0800

    test

--- ios/Classes/InstallPlugin.m
+++ ios/Classes/InstallPlugin.m
@@ -2,6 +2,20 @@
 
 @implementation InstallPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  // do nothing
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"install_plugin"
+            binaryMessenger:[registrar messenger]];
+  WechatPlugin* instance = [[WechatPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"gotoAppStore" isEqualToString:call.method]) {
+    // result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    result[@"success"];
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
 @end
